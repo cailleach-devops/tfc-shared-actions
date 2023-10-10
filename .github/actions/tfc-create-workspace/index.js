830e8785d4dbae8fbee937d12de35366a9d94de8
laura.oliveira <laura.oliveira@iteris.com.br>
2023-10-09 22:27:23 -0300

    Changes in tfc-create-workspace

--- .github/actions/tfc-create-workspace/index.js
+++ .github/actions/tfc-create-workspace/index.js
@@ -3,10 +3,8 @@ const axios = require('axios');
 
 try{
     const workSpaceName = core.getInput('workSpaceName');
-    const environmentName = core.getInput('environmentName');
     const organizationName = core.getInput('organizationName');
     const token = core.getInput('token');
-    const terraformHost = core.getInput('terraformHost');
     const projectId = core.getInput('projectId');
 
     let request = 
@@ -15,8 +13,8 @@ try{
       { 
         "attributes": 
         { 
-          "name" : workSpaceName + "-" + environmentName, 
-          "execution-mode": "local",
+          "name" : workSpaceName, 
+          "execution-mode": "remote",
           "file-triggers-enabled": false,
           "especulative-enabled": false,
           "working-directory": "terraform"
@@ -33,9 +31,7 @@ try{
       }
     };
 
-    console.log("request:" + JSON.stringify(request));
-
-    const terraformEndpoint = "https://"+terraformHost+"/api/v2/organizations/"+organizationName+"/workspaces";
+    const terraformEndpoint = "https://app.terraform.io/api/v2/organizations/"+organizationName+"/workspaces";
     const options = {
         headers: {'Content-Type': 'application/vnd.api+json',
                   'Authorization': 'Bearer '+token
@@ -44,7 +40,6 @@ try{
    // Invoking Terraform API
     axios.post(terraformEndpoint, request, options)
       .then((response) => {
-        console.log("success:"+ JSON.stringify(response.data));
         core.setOutput("workSpaceId", response.data.data.id);
       }, (error) => {
         console.error("error:"+JSON.stringify(error.response));
